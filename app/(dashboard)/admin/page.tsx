a19f1c856b8df46892d1c1ab2cae89a54730357f
AnnPoberezhna <poberezhna.anna907@gmail.com>
2025-12-06 23:23:20 +0100

    Sign Out Functionality added

--- app/(dashboard)/admin/page.tsx
+++ app/(dashboard)/admin/page.tsx
@@ -1,6 +1,14 @@
+import { authOptions } from "@/app/lib/auth";
+import { getServerSession } from "next-auth";
 
-const page = () => {
-    return <div> welcome to admin page</div>
+const page = async () => {
+    const session = await getServerSession(authOptions);
+    
+    if(session?.user){
+        return <h2 className="text-2xl">Admin page - welcome back {session?.user.username}</h2>
+    }
+
+    return <h2 className="text-2xl">Please login to see this admin page</h2>;
 };
 
 export default page;
