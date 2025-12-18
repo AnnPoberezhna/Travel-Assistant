d2cec6bfc3c541fca71209711e1ff1b29f0e8537
AnnPoberezhna <poberezhna.anna907@gmail.com>
2025-12-18 15:38:52 +0100

    Fixed: Navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -11,8 +11,8 @@ const Navbar = async () => {
 
 
   return (
-    <div className=' bg-zinc-100 py-2 border-b border-s-zinc-200 fixed w-full z-10 top-0'>
-      <div className='container flex items-center justify-between'>
+    <div className=' bg-zinc-100 py-0.5 border-b border-s-zinc-200 fixed w-full z-10 top-0'>
+      <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', padding: '0 20px', width: '100%' }}>
         <Link href='/'>
           <HandMetal />
         </Link>
